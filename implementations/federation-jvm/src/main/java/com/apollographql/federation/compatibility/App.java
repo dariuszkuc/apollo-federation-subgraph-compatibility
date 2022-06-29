4210bb0c0f35e7409888d63c7f13da456f0c15a4
dariuszkuc <9501705+dariuszkuc@users.noreply.github.com>
2022-06-29 15:42:04 -0500

    fed jvm test

--- implementations/federation-jvm/src/main/java/com/apollographql/federation/compatibility/App.java
+++ implementations/federation-jvm/src/main/java/com/apollographql/federation/compatibility/App.java
@@ -3,6 +3,9 @@ package com.apollographql.federation.compatibility;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * This is just a test to check whether workflow works.
+ */
 @SpringBootApplication
 public class App {
     public static void main(String[] args) {
