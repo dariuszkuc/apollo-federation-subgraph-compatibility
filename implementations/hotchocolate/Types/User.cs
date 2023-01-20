a5d07555af738c0ab86aa269f505a942d8f71be3
dariuszkuc <9501705+dariuszkuc@users.noreply.github.com>
2023-01-20 14:36:24 -0600

    wip

--- implementations/hotchocolate/Types/User.cs
+++ implementations/hotchocolate/Types/User.cs
@@ -6,20 +6,66 @@ namespace Products;
 [ExtendServiceType]
 public class User
 {
-    public User()
-    {
-    }
-
-    public User(string email, int? totalProductsCreated)
+    public User(string email, string? name)
     {
         Email = email;
-        TotalProductsCreated = totalProductsCreated;
+        Name = name;
+    }
+
+    public int? GetAverageProductsCreatedPerYear()
+    {
+        if (TotalProductsCreated != null && LengthOfEmployment != null)
+        {
+            return Convert.ToInt32((TotalProductsCreated * 1.0) / LengthOfEmployment);
+        }
+        return null;
     }
 
     [ID]
     [External]
-    public string Email { get; set; } = default!;
+    public string Email { get; set; }
+
+    public string? Name { get; }
 
     [External]
-    public int? TotalProductsCreated { get; set; }
+    public int? TotalProductsCreated { get; set; } = 1337;
+
+    [GraphQLIgnore]
+    public int? LengthOfEmployment { get; set; }
+
+    [External]
+    public int GetYearsOfEmployment()
+    {
+        if (LengthOfEmployment == null)
+        {
+            throw new InvalidOperationException("yearsOfEmployment should never be null - it should be populated by _entities query");
+        }
+
+        return (int)LengthOfEmployment;
+    }
+
+    [ReferenceResolver]
+    public static User? GetUserByEmail(
+        string email,
+        int? totalProductsCreated,
+        int? yearsOfEmployment,
+        Data repository)
+    {
+        User? user = repository.Users.FirstOrDefault(u => u.Email.Equals(email));
+        if (user != null)
+        {
+            Console.WriteLine("user entity, total products created = {0}", totalProductsCreated);
+            if (totalProductsCreated != null)
+            {
+                user.TotalProductsCreated = totalProductsCreated;
+            }
+
+            Console.WriteLine("user entity, years of employment = {0}", yearsOfEmployment);
+            if (yearsOfEmployment != null)
+            {
+                user.LengthOfEmployment = yearsOfEmployment;
+            }
+        }
+        return user;
+    }
 }
